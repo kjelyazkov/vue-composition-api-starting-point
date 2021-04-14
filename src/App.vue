5b41b0e2b9868586f20789269789a4135f317bb5
Kristiyan Zhelyazkov <kristiyan.zhelyazkov@flatrocktech.com>
2021-04-14 14:43:32 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<Navigation/>
-	<router-view/>
+	<div class="pt-14">
+		<transition name="fade">
+				<router-view/>
+		</transition>
+	</div>
 </template>
 <script>
 import Navigation from '@/components/Navigation'
@@ -19,5 +23,15 @@ export default {
 #app {
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,19 +1,54 @@
 <template>
 	<div>
-		<div>
-			<router-link to="/">Home</router-link>
-			<router-link to="/about">About</router-link>
+		<!-- Navigation -->
+		<div class="bg-gray-200 w-full fixed top-0 left-0 z-40">
+			<div class="mx-auto w-full max-w-screen-xl px-4 py-4 flex justify-between">
+				<div class="font-bold text-indigo-500">
+					LOGO
+				</div>	
+				<div class="sm:flex gap-4 text-gray-600 hidden">
+					<router-link to="/">Home</router-link>
+					<router-link to="/about">About</router-link>
+				</div>	
+			</div>
 		</div>
 
 
-		<div>
+		<div class="w-6 h-6 fixed top-0 right-0 my-4 mx-4 sm:hidden z-40 text-lg grid place-content-center" @click="is_open = !is_open">
+			<div v-if="is_open">
+				<i class="fas fa-times"></i>
+			</div>
+			<div v-else>
+				<i class="fas fa-bars"></i>
+			</div>
+		</div>
 
+		<!-- Mobile Navigation -->
+		<div class="fixed inset-0 bg-gray-200 z-30 transform transition-transform duration-300 ease-in-out" :class="is_open ? 'translate-x-0' : '-translate-x-full'">
+			<div class="mt-24 flex flex-col">
+				<div class="hover:bg-gray-300 py-4 px-2 text-gray-600">Item</div>
+				<div class="hover:bg-gray-300 py-4 px-2 text-gray-600">Item</div>
+				<div class="hover:bg-gray-300 py-4 px-2 text-gray-600">Item</div>
+				<div class="hover:bg-gray-300 py-4 px-2 text-gray-600">Item</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			is_open: false
+		}
+	},
+	mounted(){
+		window.addEventListener("resize", () => {
+			if(document.documentElement.clientWidth > 638) {
+				this.is_open = false
+			}
+		});
+	}
 
 }
 </script>
